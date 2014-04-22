226104df0840e876047d4cdb231beecb5ee2077f
Rodrigo Martins <rodrigo.rcm@globo.com>
2014-04-21 22:18:21 -0300

    Atualização

--- gen/com/example/appandroid/R.java
+++ gen/com/example/appandroid/R.java
@@ -47,15 +47,16 @@ public final class R {
     public static final class string {
         public static final int action_novo=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int ativo=0x7f050009;
+        public static final int ativaralerta=0x7f050009;
+        public static final int ativo=0x7f05000a;
         public static final int cadastrar=0x7f050001;
         public static final int data=0x7f050008;
         public static final int descricao=0x7f050005;
-        public static final int inativo=0x7f05000a;
+        public static final int inativo=0x7f05000b;
         public static final int local=0x7f050006;
         public static final int nome=0x7f050004;
         public static final int salvar=0x7f050007;
-        public static final int tipodealerta=0x7f05000b;
+        public static final int tipodealerta=0x7f05000c;
         public static final int title_activity_cadastro=0x7f050003;
     }
     public static final class style {

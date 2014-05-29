417954150b0335186c77bd730e0644f01dadf6a6
Lukas Roberto dos Santos <lukas.roberto@yahoo.com.br>
2014-05-29 19:13:56 -0300

    Alterada lista automatica

--- src/br/com/trabalho/main/CadastroActivity.java
+++ src/br/com/trabalho/main/CadastroActivity.java
@@ -303,23 +303,17 @@ public class CadastroActivity extends Activity {
 		Random gerar = new Random();
 		Long id = (long) 0;
 
-		for (int i = 0; i < prod.getProd().size(); i++) {
-			System.out.println(prod.getProd().get(i).getDescricao().toString());
-			id = id+1;
-			String data = "0"+id+"/05/2014";
+		for (int i = 0; i < 100; i++) {		
+			String data = gerar.nextInt(31)+"/05/2014";
 			
 			Vendas venda = new Vendas();
 			venda.setId(id);
 			venda.setData(data);
-			venda.setQtd(gerar.nextInt(26)+ "");
+			venda.setQtd(gerar.nextInt(20)+ "");
 
-			venda.setCidade(cidade.getCidades().get(i));
-			venda.setProduto(prod.getProd().get(i));
+			venda.setCidade(cidade.getCidades().get(gerar.nextInt(9)));
+			venda.setProduto(prod.getProd().get(gerar.nextInt(9)));
 			VendasDao.getInstancia().cadastrar(venda);
-			
-			
-			System.out.println(id);
-
 
 		}
 
